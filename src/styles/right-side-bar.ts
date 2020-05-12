b1486e0b2d5257a0f3670054b05d09a769a23b39
Tero Lamminpaa <tero@pop-os.localdomain>
2020-05-12 15:03:08 +0300

    Link tree structure and breadcrumb are now both functional

---
+++ src/styles/right-side-bar.ts
@@ -0,0 +1,6 @@
+import { createStyles } from "@material-ui/core";
+import theme from "./theme";
+
+export default createStyles({
+
+});
--- src/styles/tree-view.ts
+++ src/styles/tree-view.ts
@@ -2,5 +2,11 @@ import { createStyles } from "@material-ui/core";
 import theme from "./theme";
 
 export default createStyles({
-
+  treeDataLink: {
+    textDecoration: "none",
+    color: "#000",
+    "&:hover, :visited, :focus": {
+      color: "#000"
+    }
+  }
 });
