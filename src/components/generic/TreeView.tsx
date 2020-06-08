ed98c2aaf3e1c93fc47baefca6ff83811b838667
Tero Lamminpää <56538990+terolamminpaa@users.noreply.github.com>
2020-06-08 09:03:42 +0300

    Update src/components/generic/TreeView.tsx
    Co-authored-by: Lauri Lukkarinen <44664737+lauriLukkarinen@users.noreply.github.com>

--- src/components/generic/TreeView.tsx
+++ src/components/generic/TreeView.tsx
@@ -113,7 +113,7 @@ class TreeView extends React.Component<Props, State> {
         let parent = pages.find(item => item.id === mainPage!.parent);
         while (parent && parent.taxonomy_schools && parent.taxonomy_schools.find((x) => x === school.id )) {
           mainPage = parent;
-          parent = pages.find((item) => item.id === mainPage!.parent);
+          parent = pages.find(item => item.id === mainPage!.parent);
         }
         mainPages = [mainPage!];
       }
