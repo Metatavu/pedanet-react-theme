d66096d1ba84d0c96abb2b85a060d50472ea99b2
Tero Lamminpaa <tero@pop-os.localdomain>
2020-06-17 17:54:00 +0300

    Fixed tree nnot showing up

--- src/components/generic/RightSideBar.tsx
+++ src/components/generic/RightSideBar.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
 import { withStyles, WithStyles } from '@material-ui/core';
 import styles from '../../styles/right-side-bar';
-import { RelatedLink } from '../pages/PostPage';
 
 /**
  * Component props
  */
 interface Props extends WithStyles<typeof styles> {
-  links: RelatedLink[];
 }
 
 /**
@@ -41,22 +39,9 @@ class RightSideBar extends React.Component<Props, State> {
    * Component render
    */
   public render() {
-    const { links } = this.props;
     return (
       <>
         <p>Aiheeseen liittyviä linkkejä</p>
-        {
-          links.map(linkObject => {
-            if (!linkObject.link.match(/^https:\/\/|^http:\/\//g)) {
-              linkObject.link = `https://${linkObject.link}`;
-            }
-            return (
-              <div>
-                <a target="_blank" href={ linkObject.link }>{ linkObject.name }</a>
-              </div>
-            )
-          })
-        }
       </>
     )
   }
