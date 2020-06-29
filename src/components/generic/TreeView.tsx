183c95aa7c7abc85ed3c1b5b748429a202e34d29
Tero Lamminpää <56538990+terolamminpaa@users.noreply.github.com>
2020-06-29 09:02:32 +0300

    Update src/components/generic/TreeView.tsx
    Co-authored-by: Lauri Lukkarinen <44664737+lauriLukkarinen@users.noreply.github.com>

--- src/components/generic/TreeView.tsx
+++ src/components/generic/TreeView.tsx
@@ -170,6 +170,7 @@ class TreeView extends React.Component<Props, State> {
       if (!children || (!onAcademyPage && parent.academyPage)) {
         return [];
       }
+      
       const promises = children.map(node => {
         const structure = {
           key: `${ node.id }`,
