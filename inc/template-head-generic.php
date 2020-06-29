8d5aadf280d716412e9178d125c0c95d6fce4130
Tuomas Kämppi <tuomas.kamppi@metatavu.fi>
2020-06-29 15:46:24 +0300

    cleaned old theme, brought template style from twentytwenty

--- inc/template-head-generic.php
+++ inc/template-head-generic.php
@@ -1,8 +1,10 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<meta http-equiv="Content-language" content="${pageContext.request.locale.language}">
+<meta http-equiv="Content-language" content="<?php echo get_locale();?>">
+<meta property="og:locale" content="<?php echo get_locale();?>" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdn.metatavu.io/fonts/tt_norms/stylesheet.css" />
-<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url') ?>/inc/loader.css">
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url') ?>/inc/loader.css" />
+<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri() ?>" />
 <link rel="icon" type="image/png" href="<?php bloginfo('template_url') ?>/inc/favicon.png" sizes="512x512" />
 <title>Pedanet Mikkeli</title>
 <script>
