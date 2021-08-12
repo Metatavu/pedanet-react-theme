72542a0fccbf6611212024c778d5caf443af85af
Lauri Lukkarinen <44664737+lauriLukkarinen@users.noreply.github.com>
2021-08-12 12:44:29 +0300

    Updated IE check

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <?php require get_template_directory() . '/inc/template-head-generic.php'; ?>
   </head>
   <body>
-    <?php if (preg_match('(?:\b(MS)?IE\s+|\bTrident\/7\.0;.*\s+rv:)(\d+)', $_SERVER['HTTP_USER_AGENT']) === 0) { ?>
+    <?php if (preg_match('(?:\b(MS)?IE\s+|\bTrident\/7\.0;.*\s+rv:)(\d+)', $_SERVER['HTTP_USER_AGENT'])) { ?>
       <?php require get_template_directory() . '/inc/template-loader.php'; ?>
       <div id="root"></div>
       <script type="text/javascript" src="<?php echo get_template_directory_uri() ?>/dist/bundle.js"></script>
