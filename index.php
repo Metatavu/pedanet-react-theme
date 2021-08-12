8f7f4be24b36cc55b1e85bcb54a1dbba3b1ff1f1
Lauri Lukkarinen <44664737+lauriLukkarinen@users.noreply.github.com>
2021-08-12 13:26:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
     <?php require get_template_directory() . '/inc/template-head-generic.php'; ?>
   </head>
   <body>
-    <?php if (preg_match('/Trident\/([0-9]{1,}[\.0-9]{0,})/', $_SERVER['HTTP_USER_AGENT'])) { ?>
-      <?php require get_template_directory() . '/inc/template-deprecation-warning.php' ?>
-    <?php } else { ?>
+    <?php if (preg_match('~MSIE|Internet Explorer~i', $_SERVER['HTTP_USER_AGENT']) || preg_match('~Trident/7.0(; Touch)?; rv:11.0~',$_SERVER['HTTP_USER_AGENT']) === 0) { ?>
       <?php require get_template_directory() . '/inc/template-loader.php'; ?>
       <div id="root"></div>
       <script type="text/javascript" src="<?php echo get_template_directory_uri() ?>/dist/bundle.js"></script>
+    <?php } else { ?>
+      <?php require get_template_directory() . '/inc/template-deprecation-warning.php' ?>
     <?php } ?>
   </body>
 </html>
