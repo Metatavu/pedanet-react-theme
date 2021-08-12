d57010b75cc125654de8d11f67c1d78252736f2b
Lauri Lukkarinen <44664737+lauriLukkarinen@users.noreply.github.com>
2021-08-12 12:45:20 +0300

    Updated IE check

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
   </head>
   <body>
     <?php if (preg_match('(?:\b(MS)?IE\s+|\bTrident\/7\.0;.*\s+rv:)(\d+)', $_SERVER['HTTP_USER_AGENT'])) { ?>
+      <?php require get_template_directory() . '/inc/template-deprecation-warning.php' ?>
+    <?php } else { ?>
       <?php require get_template_directory() . '/inc/template-loader.php'; ?>
       <div id="root"></div>
       <script type="text/javascript" src="<?php echo get_template_directory_uri() ?>/dist/bundle.js"></script>
-    <?php } else { ?>
-      <?php require get_template_directory() . '/inc/template-deprecation-warning.php' ?>
     <?php } ?>
   </body>
 </html>
