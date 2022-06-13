72c11f1e97da3f75447314cb560cd3989c7f97b6
platonovsimeon <simeon.platonov@gmail.com>
2022-06-13 16:20:57 +0300

    Added margin front page text and changed search result categories

--- dist/localization/strings.js
+++
@@ -1,7 +0,0 @@
-import LocalizedStrings from "localized-strings";
-var strings = new LocalizedStrings({
-    en: require("./en.json"),
-    fi: require("./fi.json")
-});
-export default strings;
-//# sourceMappingURL=strings.js.map
--- dist/public-path.js
+++
@@ -1,2 +0,0 @@
-__webpack_public_path__ = THEME_PATH + "/dist/";
-//# sourceMappingURL=public-path.js.map
