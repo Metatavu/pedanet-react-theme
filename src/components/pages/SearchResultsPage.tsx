466558b4b30ae058b2f8812569761d426ad6bcec
platonovsimeon <simeon.platonov@gmail.com>
2022-08-03 15:34:36 +0300

    Fixes

--- src/components/pages/SearchResultsPage.tsx
+++ src/components/pages/SearchResultsPage.tsx
@@ -86,6 +86,7 @@ interface SearchResult {
               onChange={ event => this.setState({ query: event.target.value }) } 
               value={ this.state.query }  
               variant="outlined" 
+              aria-labelledby={ strings.search }
             />
             <Button onClick={ this.onSearch } size="large" color="primary" variant="contained">{ strings.search }</Button>
             <div style={{ marginTop: "40px" }}>
@@ -193,7 +194,7 @@ interface SearchResult {
         display: "flex",
         flexDirection: "row"
       }}>
-        { result.placeholderImage ? <img style={{ alignSelf: "center" }} src={ result.imageUrl } width={ 120 } height={ 60 }/> : <img src={ result.imageUrl } width={ 120 } height={ 120 }/> }
+        { result.placeholderImage ? <img alt={ strings.searchResultImage } style={{ alignSelf: "center" }} src={ result.imageUrl } width={ 120 } height={ 60 }/> : <img src={ result.imageUrl } width={ 120 } height={ 120 }/> }
         <div style={{ flexDirection: "column", display: "flex", marginLeft: "15px" }}>
           <p>{ result.date } - <a href={ result.url }> { result.title } </a></p>
           <p>{ result.summary }</p>
